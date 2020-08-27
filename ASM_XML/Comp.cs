865764a9cb50a51c09f6a593adff17cfd6f11a81
User <User@DESKTOP-NI>
2020-08-27 14:02:37 +0300

    Добавьте файлы проекта.

---
+++ ASM_XML/Comp.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SolidWorks.Interop.sldworks;
+using SolidWorks.Interop.swconst;
+
+namespace ASM_XML
+{
+    class Comp
+    {
+        private const double eps = 0.0000001;
+        public string format, designation, name, note, chapter, included, doc = "", type = "", rotation;
+        public int quantity;
+        public double x, y, z;
+
+
+        private static string Euler(double[] R)
+        {
+
+            double alpha, alpha2, beta, beta2, gamma, gamma2;
+
+            if (Math.Abs(Math.Abs(R[6]) - 1) > eps)
+            {
+                beta = -Math.Asin(R[6]);
+                beta2 = Math.PI - beta;
+                alpha = Math.Atan2(R[7] / Math.Cos(beta), R[8] / Math.Cos(beta));
+                alpha2 = Math.Atan2(R[7] / Math.Cos(beta2), R[8] / Math.Cos(beta2));
+                gamma = Math.Atan2(R[3] / Math.Cos(beta), R[0] / Math.Cos(beta));
+                gamma2 = Math.Atan2(R[3] / Math.Cos(beta2), R[0] / Math.Cos(beta2));
+            }
+            else
+            {
+                gamma = 0;
+            }
+            if (Math.Abs(R[6] + 1) < eps)
+            {
+                beta = Math.PI / 2;
+                alpha = gamma + Math.Atan2(R[1], R[2]);
+            }
+            else
+            {
+                beta = -Math.PI / 2;
+                alpha = -gamma + Math.Atan2(-R[1], -R[2]);
+            }
+
+            return Math.Round(alpha * 180 / Math.PI, 2) + "; " + Math.Round(beta * 180 / Math.PI, 2) + "; " + Math.Round(gamma * 180 / Math.PI, 2);
+        }
+
+        public static List<Comp> GetColl(AssemblyDoc swAssy)
+        {
+
+            Comp component;
+            List<Comp> coll;
+            object[] comps;
+            Component2 comp;
+            ModelDoc2 compDoc;
+            CustomPropertyManager prpMgr;
+            ModelDocExtension swModelDocExt;
+            string configuration;
+            double[] aTrans;
+            string valOut;
+
+            coll = new List<Comp>();
+
+            swAssy.ResolveAllLightWeightComponents(false);
+
+            comps = swAssy.GetComponents(true);
+            Console.WriteLine("GetColl");
+            Console.WriteLine(comps.Length);
+
+
+            for (int i = 0; i < comps.Length; i++)
+            {
+                component = new Comp();
+                comp = (Component2)comps[i];
+                aTrans = comp.Transform2.ArrayData;
+                compDoc = comp.GetModelDoc2();
+
+                if ((comp.GetSuppression() != (int)swComponentSuppressionState_e.swComponentSuppressed) & (comps[i] != null))
+                {
+                    configuration = (string)comp.ReferencedConfiguration;
+                    swModelDocExt = (ModelDocExtension)compDoc.Extension;
+                    prpMgr = (CustomPropertyManager)swModelDocExt.get_CustomPropertyManager(configuration);
+
+                    prpMgr.Get4("Формат", true, out valOut, out _);
+                    component.format = valOut;
+                    prpMgr.Get4("Обозначение", true, out valOut, out _);
+                    component.designation = valOut;
+                    prpMgr.Get4("Наименование", true, out valOut, out _);
+                    component.name = valOut;
+                    prpMgr.Get4("Примечание", true, out valOut, out _);
+                    component.note = valOut;
+                    prpMgr.Get4("Раздел", true, out valOut, out _);
+                    component.chapter = valOut;
+                    prpMgr.Get4("Перв.Примен.", true, out valOut, out _);
+                    component.included = valOut;
+
+                    if ((component.chapter == "Стандартные изделия") | (component.chapter == "Прочие изделия"))
+                    {
+                        prpMgr.Get4("Документ на поставку", true, out valOut, out _);
+                        component.doc = valOut;
+                        component.type = component.name.Substring(0, component.name.IndexOf((char)32));
+                    }
+                    component.rotation = Euler(aTrans);
+                }
+                coll.Add(component);
+
+            }
+
+            foreach (Comp k in coll)
+            {
+                if (k.chapter != "Сборочные единицы" | k.chapter != "Детали" | k.chapter != "Документация" | k.chapter != "Комплекты")
+                {
+                    k.format = "";
+                    k.designation = "";
+                }
+                if (k.format.Contains("*)"))
+                {
+                    k.note = k.format.Substring(2);
+                    k.format = "*)";
+                }
+            }
+
+         return coll;
+        }
+    }
+}
---
+++ ASM_XML/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using SolidWorks.Interop.sldworks;
+using SolidWorks.Interop.swconst;
+
+namespace ASM_XML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var progId = "SldWorks.Application.27";
+
+            var progType = System.Type.GetTypeFromProgID(progId);
+
+            var swApp = System.Activator.CreateInstance(progType) as ISldWorks;
+            swApp.Visible = false;
+            Console.WriteLine(swApp.RevisionNumber());
+            Console.CursorSize = 100;
+            ModelDoc2 swModel;
+            ModelDocExtension swModelDocExt;
+            AssemblyDoc swAssy;
+            Component2 swComp;
+            DrawingDoc Part;
+            List<Comp> coll;
+
+            int errors = 0;
+            int warnings = 0;
+            string fileName;   // GetOpenFileName
+            Dictionary<string, string> Dict, Drw;
+            string projekt_path, key, pathName;
+            string[] сonfNames;
+            object[] Comps;
+
+            fileName = swApp.GetOpenFileName("File to SLDASM", "", "SLDASM Files (*.SLDASM)|*.SLDASM|", out _, out _, out _);
+            //Проверяем путь
+            if (fileName == "")
+            {
+                swApp.ExitApp();
+                return;
+            }
+            swModel = (ModelDoc2)swApp.OpenDoc6(fileName, (int)swDocumentTypes_e.swDocASSEMBLY, (int)swOpenDocOptions_e.swOpenDocOptions_Silent, "", ref errors, ref warnings);
+
+            //Проверяем открыта сборка или нет
+            if ((swModel.GetType() != 2) | (swModel == null))
+            {
+                swApp.SendMsgToUser2("Откройте сборку", 4, 2);
+                swApp.ExitApp();
+                return;
+            }
+            swAssy = (AssemblyDoc)swModel;
+            coll=Comp.GetColl(swAssy);
+        }
+    }
+}
